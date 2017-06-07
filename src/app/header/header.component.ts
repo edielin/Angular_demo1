bdd170f3de815573f78fb443bf3bc26fd9e3a9b4
edie <edie.lin@kaisquare.com.tw>
2017-06-08 02:32:51 +0800

    示範 屬性繫結 的進階用法 [innerHTML]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,9 @@ export class HeaderComponent implements OnInit {
   
   imgSrc = '/assets/images/logo.png';
   imgTitle = 'The Will Will Web';
-  h1Title ='The Will Will Will Web';
+  h1Title = 'The Will Will Will Web';
+
+  h3innerHTML = '記載著 <span style="color: #ff0" class="test">Will</span> 在網路世界的學習心得與技術分享';
 
   constructor() { }
 
