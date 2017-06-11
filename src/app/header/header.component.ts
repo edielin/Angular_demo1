f21828ffc01e3d9bb5bd8f2539fd8f56eac9dba9
edie <edie.lin@kaisquare.com.tw>
2017-06-11 12:34:16 +0800

    5 練習Angular 事件繫結 (eventName)="someMethod($event)" ; on-eventName="someMethod($event)"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,13 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+
   imgSrc = '/assets/images/logo.png';
   imgTitle = 'The Will Will Web';
   h1Title = 'The Will Will Will Web';
 
   h3innerHTML = '記載著 <span style="color: #ff0" class="test">Will</span> 在網路世界的學習心得與技術分享';
 
+  changeH1Title () {
+    this.h1Title = '[click Event Action! 1]';
+  }
   constructor() { }
 
   ngOnInit() {
