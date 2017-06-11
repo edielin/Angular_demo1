6e98b6e52d00711c4ebc98d526e850e93b2db11b
edie <edie.lin@kaisquare.com.tw>
2017-06-11 22:07:53 +0800

    示範 $event 的使用方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   keyWord = '';
+
+  clearKeyWord($event: KeyboardEvent) {
+    console.log($event);
+    if ($event.keyCode === 27) {
+      this.keyWord = '';
+    }
+  }
 }
